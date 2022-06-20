86638cba180426988dae77174eaf3de1f05a4701
Anaele Nnaemeka <anaelennaemeka@gmail.com>
2022-06-20 10:41:43 +0100

    cors issues 4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+module.exports = {
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "http://api.football-data.org/:path*",
+			},
+		];
+	},
+};
